2026b602dc2eb4dd490b66ebd84130a09ed90bfe
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-01-01 20:50:15 -0600

    Añadiendo modo escuro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
    mode: 'jit', //Esta opcion activa el motor JIT o Just In Time
    purge: ['./*.html'], //Analiza las classes de Tailwind para solo usar esas en el proyecto final
+   darkMode: 'media', //Activar el modo oscuro
    content: [],
    theme: {
       extend: {},
