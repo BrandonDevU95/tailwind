d5d050fa8ee7c8d87d0812abb73ad0451229f884
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2022-01-01 20:38:34 -0600

    Pagina adaptable

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+   mode: 'jit', //Esta opcion activa el motor JIT o Just In Time
+   purge: ['./*.html'], //Analiza las classes de Tailwind para solo usar esas en el proyecto final
+   content: [],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
+};
